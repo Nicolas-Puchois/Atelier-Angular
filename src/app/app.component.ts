b177b6c23fbfedad1b865e6dbb0e7748add51aa8
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-17 15:04:09 +0200

    feat(): create "main" component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TotoComponent } from './toto/toto.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { IncrementeComponent } from './incremente/incremente.component';
+import { DecrementeComponent } from './decremente/decremente.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    TotoComponent,
+    IncrementeComponent,
+    DecrementeComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Atelier-Angular';
+
 }
